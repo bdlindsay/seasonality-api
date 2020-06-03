8eea01c9cc1e8213c973e9e2954d1d26bad00b9a
bdlind <brett.d.lindsay@monsanto.com>
2020-06-03 15:41:08 -0500

    cors and compression

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const app = require("express")();
+const cors = require("cors");
+const compression = require("compression");
 const port = 3000;
 
-app.get("/v1/seasonality-data", (req, res) =>
-  res.json(require("./globalGrowingData.json"))
-);
+app.use(compression());
+app.use(cors());
+
+app.get("/v1/seasonality-data", (req, res) => {
+  res.status(200).json(require("./globalGrowingData.json"));
+});
 
 app.listen(port, () => {
   console.log("Listening on port", port);
